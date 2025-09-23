37f5de5da1d9ed79439385259cca1601e2c0a897
NayeyYe <Nayey.Ye@outlook.com>
2025-09-23 22:18:23 +0800

    fix: 修复关联表错误问题

--- database/schema/tables/books/book_tags.sql
+++ database/schema/tables/books/book_tags.sql
@@ -1,6 +1,9 @@
 USE bookmanage;
 DROP TABLE IF EXISTS book_tags;
 CREATE TABLE IF NOT EXISTS book_tags (
-    tag_id VARCHAR(50) PRIMARY KEY,
-    tag_name VARCHAR(100) NOT NULL UNIQUE
+    book_id VARCHAR(50),
+    tag_id VARCHAR(50),
+    PRIMARY KEY (book_id, tag_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
 );
